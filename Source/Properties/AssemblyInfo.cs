df891870e8cbdfe698bfd16ef4fa5cff3af92139
cake-pie <cake-pie@users.noreply.github.com>
2019-04-13 03:21:38 -0700

    version

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle ("KerbalStatusEffects")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("KerbalStatusEffects")]
+[assembly: AssemblyCopyright ("Copyright © 2019")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: AssemblyVersion ("0.1.0.0")]
+[assembly: KSPAssembly("KerbalStatusEffects", 0, 1, 0)]
